451265594eecf2de75201b162e7c9d638d13fe34
T <114290092+LTarfLieT@users.noreply.github.com>
2023-02-28 14:19:08 +0800

    Update TestShape.java

--- TestShape.java
+++ TestShape.java
@@ -1,16 +1,14 @@
-
 class TestShape {
 
         public static void main(String[] args) {
 
             Circle circle = new Circle(9);  //Circle Area
-            //Console.writeline is not a java method, it is .net
-            System.out.println(circle.calcArea());
+            System.out.println("Circle area = " +circle.calcArea());
             Rectangle rectangle = new Rectangle(8, 5);  //Rectangle Area
             System.out.println("Rectangle area = " + rectangle.calcArea());
             Triangle triangle = new Triangle(8, 5); //Triangle Area
             System.out.println("Triangle area = " + triangle.calcArea());
+      
         
         }
     }
-   
